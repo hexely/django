54890eee2b2008d690fbb3e8145ee1785f1e184f
hexely <ihistos91@gmail.com>
2023-09-02 16:51:06 +0300

    add homework

--- catalog/views.py
+++ catalog/views.py
@@ -10,8 +10,9 @@ def index(request):
 def contacts(request):
     if request.method == "POST":
         name = request.POST.get('name')
+        number_phone = request.POST.get('number_phone')
         email = request.POST.get('email')
         message = request.POST.get('message')
         all_ = request.POST
-        print(f'{name}, ({email}): {message}')
+        print(f'{name}, ({email}, {number_phone}): {message}')
     return render(request, 'catalog/contacts.html')
