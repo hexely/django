8472f3fca39d34373314719ac65f9d3a91a8ac5e
hexely <ihistos91@gmail.com>
2023-10-24 21:03:59 +0300

    add homework 22.2 authenticated

--- catalog/urls.py
+++ catalog/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path('', IndexListView.as_view(), name='index2'),
     path('contacts2/', contacts, name='contacts2'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
-    path('create/',  login_required(ProductCreateView.as_view()), name='create_product'),
-    path('update/<int:pk>/',  login_required(ProductUpdateView.as_view()), name='update_product'),
-    path('create_v/',  login_required(VersionCreateView.as_view()), name='create_version'),
-    path('update_v/',  login_required(VersionUpdateView.as_view()), name='update_version')
+    path('create/', login_required(ProductCreateView.as_view()), name='create_product'),
+    path('update/<int:pk>/', login_required(ProductUpdateView.as_view()), name='update_product'),
+    path('create_v/', login_required(VersionCreateView.as_view()), name='create_version'),
+    path('update_v/', login_required(VersionUpdateView.as_view()), name='update_version')
 ]
